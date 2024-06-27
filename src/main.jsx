58a06a7d58a09812fc50b18431d4b794f5aababe
José Rafael <joserafaeltineog@gmail.com>
2024-06-27 13:29:13 -0400

    Reemplazando hashroute por browserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter basename='/El-Futuro-del-jacinto'>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
